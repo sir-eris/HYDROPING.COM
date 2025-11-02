1fed0951e8c2389ebe77f1d83bd75296c107c5af
Eris Verne <ramtinmir.development@gmail.com>
2025-11-02 00:20:33 -0700

    pushing...

--- src/app/order/thank-you/page.js
+++ src/app/order/thank-you/page.js
@@ -1,8 +1,9 @@
+"use client";
+
 import { Suspense } from "react";
 import SuccessPage from "../../components/SuccessPage";
 
 export default function ThankYou() {
-
   return (
     <Suspense fallback={<div>Loading details...</div>}>
       <SuccessPage />
